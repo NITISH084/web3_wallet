b6a2bd0cb4d25bc482dd0fbe54c6e28a7e9e0344
NITISH084 <imnitishkumar04@gmail.com>
2025-05-08 19:36:17 +0530

    few improv nav

--- parts/Nav.tsx
+++ parts/Nav.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from "@/components/ui/Themetoggle"
 
 const Nav = () => {
   return (
-    <div className="fixed mb-2 w-screen p-2 text-2xl bg-zinc-300 dark:bg-gray-700 text-black dark:text-white flex flex-row justify-between items-center">
+    <div className="fixed mb-2 w-screen p-2 text-2xl bg-zinc-300 dark:bg-gray-700 text-black dark:text-white flex flex-row justify-between items-centerz-20">
       <p>Web3 Wallet</p>
       <ThemeToggle/>
     </div>
